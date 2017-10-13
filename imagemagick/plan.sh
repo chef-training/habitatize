642f9c6d469bc031e69b3b9ca4136f39f067d744
Franklin Webber <franklin.webber@gmail.com>
2017-10-13 09:18:51 -0500

    Fixes the imagemagick dependencies
    The is a problem with the studio where build deps hang around and become
    runtime deps. It hid the fact that imagemagick needs libjpeg-turbo as a runtime
    dep.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- imagemagick/plan.sh
+++ imagemagick/plan.sh
@@ -7,14 +7,14 @@ pkg_license=('Apache2')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=http://www.imagemagick.org/download/releases/ImageMagick-${pkg_version}.tar.xz
 pkg_shasum=da2f6fba43d69f20ddb11783f13f77782b0b57783dde9cda39c9e5e733c2013c
-pkg_deps=(core/glibc core/zlib core/libpng core/xz core/gcc-libs)
-pkg_build_deps=(core/zlib core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/glibc core/pkg-config core/libjpeg-turbo)
+pkg_deps=(core/glibc core/zlib core/libpng core/xz core/gcc-libs core/libjpeg-turbo)
+pkg_build_deps=(core/zlib core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/glibc core/pkg-config)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include/ImageMagick-6)
 pkg_lib_dirs=(lib)
 pkg_dirname=ImageMagick-${pkg_version}
 
 do_build() {
-    CC="gcc -std=gnu99" ./configure --prefix=$pkg_prefix '--with-jpeg=yes'
+    CC="gcc -std=gnu99" ./configure --prefix=$pkg_prefix
     make
 }
